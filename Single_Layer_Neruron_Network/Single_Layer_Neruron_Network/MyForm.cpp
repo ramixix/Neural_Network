66623929bdee68a07c5c638ccbce329032d33c03
ramixix <hello@domain.com>
2021-12-09 00:01:25 -0800

    making an empty project and add an empty form

---
+++ Single_Layer_Neruron_Network/Single_Layer_Neruron_Network/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	SingleLayerNeruronNetwork::MyForm form;
+	Application::Run(% form);
+}
