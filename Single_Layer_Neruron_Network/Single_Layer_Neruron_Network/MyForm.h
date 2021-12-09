d09d3b45bcd43da103805ac6501811ea1bcbadba
ramixix <hello@domain.com>
2021-12-09 01:47:29 -0800

    add coordinate plane with horizontal and vertical lines

--- Single_Layer_Neruron_Network/Single_Layer_Neruron_Network/MyForm.h
+++ Single_Layer_Neruron_Network/Single_Layer_Neruron_Network/MyForm.h
@@ -34,6 +34,8 @@ namespace SingleLayerNeruronNetwork {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ Coordinate_plane_PictureBox;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +50,47 @@ namespace SingleLayerNeruronNetwork {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->Coordinate_plane_PictureBox = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Coordinate_plane_PictureBox))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// Coordinate_plane_PictureBox
+			// 
+			this->Coordinate_plane_PictureBox->BackColor = System::Drawing::SystemColors::ControlLightLight;
+			this->Coordinate_plane_PictureBox->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->Coordinate_plane_PictureBox->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->Coordinate_plane_PictureBox->Location = System::Drawing::Point(0, 27);
+			this->Coordinate_plane_PictureBox->Name = L"Coordinate_plane_PictureBox";
+			this->Coordinate_plane_PictureBox->Size = System::Drawing::Size(800, 600);
+			this->Coordinate_plane_PictureBox->TabIndex = 0;
+			this->Coordinate_plane_PictureBox->TabStop = false;
+			this->Coordinate_plane_PictureBox->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::Coordinate_plane_PictureBox_Paint);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1184, 641);
+			this->Controls->Add(this->Coordinate_plane_PictureBox);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"MyForm";
+			this->Text = L"Single Layer Network";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Coordinate_plane_PictureBox))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+//##################################################################################################################################################################################################
+	// Draw the horizontal and vertical lines on coordinate plane 
+	private: System::Void Coordinate_plane_PictureBox_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+		Pen^ pen_color = gcnew Pen(Color::Black, 2.0f);
+		int center_width = (int)(Coordinate_plane_PictureBox->Width / 2);
+		int center_height = (int)(Coordinate_plane_PictureBox->Height / 2);
+		// draw vertical line
+		e->Graphics->DrawLine(pen_color, center_width, 0, center_width, Coordinate_plane_PictureBox->Height);
+		// draw horizontal line
+		e->Graphics->DrawLine(pen_color, 0, center_height, Coordinate_plane_PictureBox->Width, center_height);
+	}
 	};
 }
